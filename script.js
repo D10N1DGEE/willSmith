0f4f11ce29d3f28670b1c9843cf4b1ce71baa022
D10N1DGEE <95976533+D10N1DGEE@users.noreply.github.com>
2023-04-04 17:34:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger');
+const ul = document.querySelector('ul');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    ul.classList.toggle('active');
+});
